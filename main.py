764d85e8dd7ff993cece942a58c43dc275739dc9
objektwerks <objektwerks@runbox.com>
2025-05-04 09:25:26 -0400

    formatted main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def main():
             "siblings": [1, 2, 3, 4],
         }
     )
-    print(f"*** join: {df.join(df2, on="name", how="left")}")
+    print(f"*** join: {df.join(df2, on='name', how='left')}")
 
     df3 = pl.DataFrame(
         {
@@ -109,7 +109,7 @@ def main():
             "height": [1.76, 1.6, 1.66, 1.8],  # (m)
         }
     )
-    print(f"*** concat: {pl.concat([df, df3], how="vertical")}")
+    print(f"*** concat: {pl.concat([df, df3], how='vertical')}")
 
 
 if __name__ == "__main__":
