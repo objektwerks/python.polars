52da8bf2ac6ae902d79d310a2fceecb2b9e60a13
objektwerks <objektwerks@runbox.com>
2025-05-04 07:46:43 -0400

    refactored main

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 def main():
-    print("Hello from python-polars!")
+  import polars as pl
+  import datetime as dt
+
+  df = pl.DataFrame(
+      {
+          "name": ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
+          "birthdate": [
+              dt.date(1997, 1, 10),
+              dt.date(1985, 2, 15),
+              dt.date(1983, 3, 22),
+              dt.date(1981, 4, 30),
+          ],
+          "weight": [57.9, 72.5, 53.6, 83.1],  # (kg)
+          "height": [1.56, 1.77, 1.65, 1.75],  # (m)
+      }
+  )
+
+  print(df)
 
 if __name__ == "__main__":
     main()
