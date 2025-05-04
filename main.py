f90ea8862f6cad1f1f8f3a89f669e6a51c25b618
objektwerks <objektwerks@runbox.com>
2025-05-04 08:59:49 -0400

    enhanced main.py

--- main.py
+++ main.py
@@ -94,6 +94,21 @@ def main():
     )
     print(df.join(df2, on="name", how="left"))
 
+    df3 = pl.DataFrame(
+        {
+            "name": ["Ethan Edwards", "Fiona Foster", "Grace Gibson", "Henry Harris"],
+            "birthdate": [
+                dt.date(1977, 5, 10),
+                dt.date(1975, 6, 23),
+                dt.date(1973, 7, 22),
+                dt.date(1971, 8, 3),
+            ],
+            "weight": [67.9, 72.5, 57.6, 93.1],  # (kg)
+            "height": [1.76, 1.6, 1.66, 1.8],  # (m)
+        }
+    )
+    print(pl.concat([df, df3], how="vertical"))
+
 
 if __name__ == "__main__":
     main()
